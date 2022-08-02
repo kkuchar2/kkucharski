6a531ee2e22336e456a3404f92ffb278ccaa5539
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2022-08-02 11:17:52 +0200

    Update firebase variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,20 +2,24 @@ import { getAnalytics } from "firebase/analytics";
 import {FirebaseApp, initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-    authDomain: process.env.FIREBASE_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PUBLIC_PROJECT_ID,
-    appId: process.env.FIREBASE_PUBLIC_APP_ID,
-    storageBucket: process.env.FIREBASE_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_PUBLIC_MESSAGING_SENDER_ID,
-    measurementId: process.env.FIREBASE_PUBLIC_MEASUREMENT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_APP_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_MESSAGING_SENDER_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_MEASUREMENT_ID,
 };
 
 export const initFirebase = () => {
+    console.log('DEBUG: ', process.env);
+    console.log('DEBUG: Firebase config: ', firebaseConfig);
+
     if (firebaseConfig?.projectId) {
         // Initialize Firebase
         return initializeApp(firebaseConfig);
     }
+    console.log('DEBUG: Fail to initialize Firebase');
     return null;
 };
 
