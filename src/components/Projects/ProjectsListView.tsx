6fa9ed89d056764a3659f303112d23cd48bfae95
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2025-04-30 00:42:17 +0200

    Add mask styling to projects

--- src/components/Projects/ProjectsListView.tsx
+++ src/components/Projects/ProjectsListView.tsx
@@ -10,7 +10,7 @@ export const ProjectsListView = () => {
         <div className={styles.title}>
             {'Projects'}
         </div>
-        <div className={'mt-[100px] grid grid-cols-1 gap-[50px] md:grid-cols-2 lg:flex-row 2xl:grid-cols-3'}>
+        <div className={'mt-[100px] grid grid-cols-1 gap-[30px] md:grid-cols-2 lg:flex-row 2xl:grid-cols-3'}>
             {portfolioConfig.projects.map((project, index) => <ProjectItem project={project} key={index}/>)}
         </div>
     </div>; 
