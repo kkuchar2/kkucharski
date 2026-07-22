f350562aa5664893e670d4e9f9d276e645b7e4da
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2026-07-22 09:04:11 +0200

    Remove GrowingTree component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Experience } from 'components/Experience/Experience'
-import { GrowingTree } from 'components/GrowingTree/GrowingTree'
 import { Intro } from 'components/Intro/Intro'
 import { JsonLdScripts } from 'components/SEO/JsonLdScripts'
 
@@ -9,7 +8,6 @@ export default function Index() {
   return (
     <div className={styles.page}>
       <JsonLdScripts />
-      <GrowingTree />
       <div className={styles.shell}>
         <Intro />
         <Experience />
